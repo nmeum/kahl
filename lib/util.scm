4a69ca441306a18f72847c7ee679704cbf7ed91f
Sören Tempel <soeren+git@soeren-tempel.net>
2021-03-06 17:46:28 +0100

    Cleanup util.scm

--- lib/util.scm
+++ lib/util.scm
@@ -49,24 +49,3 @@
                      (bytevector-u8-ref bv index)
                      (shift-proc index)))
                 (range size)))))
-
-;;> Converts BARE variable-length unsigned integer.
-(define (bytevector->uint bv)
-  (if (> (bytevector-length bv) 10)
-    (error "maximum length of encoded uint is 10 bytes")
-    (list->bits
-      (bytevector-fold-right
-        (lambda (x ys)
-          (let* ((lst    (bits->list x))
-                 (no-msb (if (msb-set? x)
-                           (drop-right lst 1)
-                           lst)))
-            (append no-msb ys)))
-        '() bv))))
-
-;;> Converts BARE variable-length signed integer.
-(define (uint->number n)
-  (let ((x (arithmetic-shift n -1)))
-    (if (zero? (first-set-bit n))
-      (bitwise-eqv x)
-      x)))
