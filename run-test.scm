d7cf53eff530e8a1bf40d12cbf8010de543c0da1
Sören Tempel <soeren+git@soeren-tempel.net>
2021-03-06 13:11:22 +0100

    Expand test cases

--- run-test.scm
+++ run-test.scm
@@ -2,9 +2,12 @@
 
 (import (scheme base) (kahl) (chibi test))
 
-(define (test-parse parser bv)
-  (let* ((stream (bytevector->parse-stream bv))
-         (result (parse parser stream)))
-    result))
+(define (test-parse expected parser bv)
+  (define (%test-parse parser bv)
+    (let* ((stream (bytevector->parse-stream bv))
+           (result (parse parser stream)))
+      result))
+
+  (test expected (%test-parse parser bv)))
 
 (include "tests/integer.scm")
--- tests/integer.scm
+++ tests/integer.scm
@@ -1,4 +1,10 @@
 (test-group "integer"
-  (test "parse variable length uint"
-    300
-    (test-parse parse-var-uint #u8(#xac #x2))))
+  (test-group "parse variable length uints"
+    (test-parse 0   parse-var-uint #u8(0))
+    (test-parse 300 parse-var-uint #u8(#xac #x2)))
+
+  (test-group "parse variable length ints"
+    (test-parse 0  parse-var-int #u8(0))
+    (test-parse -1 parse-var-int #u8(1))
+    (test-parse 1  parse-var-int #u8(2))
+    (test-parse -2 parse-var-int #u8(3))))
